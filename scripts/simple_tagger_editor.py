dcf2715be3cf5d28aacbc238602c3712d38eb99d
DKZ <davidkingzyb@163.com>
2024-03-09 14:30:26 +0800

    save all

--- scripts/simple_tagger_editor.py
+++ scripts/simple_tagger_editor.py
@@ -16,23 +16,25 @@ def on_ui_tabs():
             with gr.Column():
                 gallery=gr.Gallery().style(columns=3)
             with gr.Column():
-                add_tb=gr.Textbox(label="Additional tags (split by comma) (first letter is comma add at the end)")
+                add_tb=gr.Textbox(label="Additional tags (split by comma) (first letter is $ add at the end)")
                 remove_tb=gr.Textbox(label="Exclude tags (split by comma) (regular expression support `(` escaped as `\(` )")
                 tagger_tb=gr.Textbox(label="Tagger")
                 save_btn=gr.Button(value="Save",variant="primary")
+                save_all_btn=gr.Button(value="Save All")
         load_btn.click(fn=loadBtnClick,inputs=dir_tb,outputs=gallery)
         gallery.select(gallerySelect,None,tagger_tb)
         save_btn.click(fn=saveClick,inputs=tagger_tb)
+        save_all_btn.click(fn=saveAllClick)
         add_tb.blur(fn=addBlur,inputs=add_tb)
         remove_tb.blur(fn=removeBlur,inputs=remove_tb)
         return [(ui_component, "Tagger Editor", "taggers_editor_tab")]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
 
-add=''
+adds=[]
 def addBlur(i):
-    global add
-    add=i
+    global adds
+    adds=i.split(',')
 
 removes=[]
 def removeBlur(i):
@@ -52,54 +54,69 @@ def loadBtnClick(dir_tb):
             pngs.append(f)
     result=[os.path.join(dir_tb,l) for l in pngs]
     return result
+
+def saveAllClick():
+    global pngs
+    global dir
+    for png in pngs:
+        file_name=os.path.join(dir,png.split('.')[0]+'.txt')
+        with open(file_name,'r') as f:
+            t=f.read()
+        result=_doRemove(t)
+        result=_doAdd(result)
+        result=_format(result)
+        if result!=t:
+            print('save all ',file_name)
+            with open(file_name,'w') as ff:
+                ff.write(result)
+
+def _doAdd(tag):
+    global adds
+    result=tag
+    front=''
+    for a in adds:
+        r=a.strip()
+        if r and r[0]=='$' and r[1:] not in result:
+            result=result+', '+r[1:]
+        elif r and r[0]!='$' and r not in result:
+            front=front+', '+r
+    result=front[2:]+', '+result
+    return result
+
+def _doRemove(tag):
+    global removes
+    result=tag
+    for s in removes:
+        r=re.compile(s.strip())
+        result=re.sub(r,'',result)#result.replace(r,'')
+    return result
+
+def _format(tag):
+    tags=tag.split(',')
+    result=[]
+    for t in tags:
+        if t.strip()!='':
+            result.append(t.strip())
+    return ', '.join(result)
+
+    
     
 file_name=''   
 def gallerySelect(evt:gr.SelectData):
     global pngs
     global dir
     global file_name
-    global add
-    global removes
     file_name=pngs[evt.index].split('.')[0]
     print('select',file_name)
     with open(os.path.join(dir,file_name+'.txt')) as f:
         t=f.read()
-    result=t
-    for s in removes:
-        r=re.compile(s.strip())
-        result=re.sub(r,'',result)#result.replace(r,'')
 
-    adds=[]
-    is_add2front=True
-    if add and add[0]==',':
-        adds=add[1:].split(',')
-        is_add2front=False
-    elif add:
-        adds=add.split(',')
-        adds.reverse()
-    for a in adds:
-        r=a.strip()
-        if r not in result:
-            if is_add2front:
-                result=r+', '+result
-            else:
-                result=result+', '+r
-
-    # remove comma 
-    result=re.sub(r',\s+,',',',result)
-    result=re.sub(r',\s+,',',',result)# cause by prev line
-    result=re.sub(r',,',',',result)
-    result=re.sub(r',,',',',result)
-    result=result.strip()
-    if result[0]==',':
-        result=result[1:]
-    if result[-1:]==',':
-        result=result[:-1]
-    result=result.strip()
-
+    result=_doRemove(t)
+    result=_doAdd(result)
+    result=_format(result)
 
     if result!=t:
-        print('replace',file_name)
+        print('edit ',file_name)
         with open(os.path.join(dir,file_name+'.txt'),'w') as ff:
             ff.write(result)
     return result
